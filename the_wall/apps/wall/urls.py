1509effe24e518a4446ce70591ce1ebf13b03538
EisnesIhsakak <54191983+EisnesIhsakak@users.noreply.github.com>
2019-10-16 13:24:08 -0500

    Add files via upload

---
+++ the_wall/apps/wall/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.wall),
+    path('post/', views.post),
+    path('comment/', views.comment),
+    path('delete/', views.delete)
+]
---
+++ the_wall/apps/wall/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+
+def wall(request):
+    content = {
+        "messages": Message.objects.all().order_by('-created_at'),
+        "comments": Comment.objects.all()
+    }
+    return render(request, 'windex.html', content)
+
+
+def post(request):
+    Message.objects.post_message(
+        request.POST['message'],
+        request.session['id']
+    )
+    return redirect('/wall/')
+
+
+def comment(request):
+    Comment.objects.post_comment(
+        request.POST['comment'],
+        request.session['id'],
+        request.POST['message_id']
+    )
+    return redirect('/wall/')
+
+
+def delete(request):
+    Comment.objects.delete_comment(request.POST['comment_id'])
+    return redirect('/wall/')
